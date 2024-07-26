6dc897b961b481f7ad6b46c792a0e68c72e4691e
Ricardo Hernandez <ricaroherniag@gmail.com>
2024-07-26 05:02:01 -0700

    changed service-worker.js to sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,28 @@
 // service-worker.js
-importScripts("https://cdn.pushalert.co/sw-72597.js");
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker instalado');
+  prompt("Service worker instalado");
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './styles.css',
+        './app.js',
+        './manifest.json',
+        './assets/icons/icon-36x36.png', // Añadir aquí todos los recursos que deseas cachear
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activado');
+  prompt("Service worker activado");
+  event.waitUntil(
+    // Aquí puedes colocar código para inicializar el Service Worker, como sincronizar datos
+    Promise.resolve() // Solo para evitar el error de waitUntil
+  );
+});
 
