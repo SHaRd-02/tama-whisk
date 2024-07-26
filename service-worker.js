f0d07b8a1b4ee3605a3abcce7213c8df6caacd85
Ricardo <115340445+SHaRd-02@users.noreply.github.com>
2024-07-26 04:29:22 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,3 @@
 // service-worker.js
 importScripts("https://cdn.pushalert.co/sw-72597.js");
-self.addEventListener('install', function(event) {
-  console.log('Service Worker instalado');
-  event.waitUntil(
-    caches.open('my-cache').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './styles.css',
-        './app.js',
-        './manifest.json',
-        './assets/icons/icon-36x36.png', // Añadir aquí todos los recursos que deseas cachear
-      ]);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker activado');
-  event.waitUntil(
-    // Aquí puedes colocar código para inicializar el Service Worker, como sincronizar datos
-    Promise.resolve() // Solo para evitar el error de waitUntil
-  );
-});
 
